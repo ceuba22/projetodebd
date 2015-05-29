dcee5e8f4368610d3f927d60415fc7b35380592e
LUIZ <LUIZ@LUIZ-PC.home>
2015-05-29 00:27:11 -0300

    Comitando mudanças!

--- ActivityPlanner/src/br/com/activity/atividade/entidade/Atividade.java
+++ ActivityPlanner/src/br/com/activity/atividade/entidade/Atividade.java
@@ -1,5 +1,33 @@
 package br.com.activity.atividade.entidade;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+
 public class Atividade {
+	
+	
+	@Column(name="ID")
+	long id;
+	
+	@Column(name="NOME")
+	String nome;
+	
+	@Column(name="DESCRICAO")
+	String descricao;
+	
+	@Column(name="CRIADO_EM")
+	private Date criadoEm;
+	
+	@Column(name="PESO")
+	int peso;
+	
+	@Column(name="TEMPO_EXECUCAO")
+	Date tempoExecucao;
+	
+	
+	
+	
+	
 
 }
